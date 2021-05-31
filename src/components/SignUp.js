b042e2767069a9dad84d5c814c8afd9724c718b0
JonathanGab <jogaborit@hotmail.com>
2021-05-31 14:24:01 +0200

    add nav bar and create sign up page

---
+++ src/components/SignUp.js
@@ -0,0 +1,102 @@
+/* eslint-disable */
+import axios from "axios"
+import { useForm } from "react-hook-form";
+
+export default function SignUp() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (form) => {
+    axios.post('http://localhost:3001/profil/user', form)
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+  }
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Créer votre compte</h2>
+        </div>
+        <form onSubmit={handleSubmit(onSubmit)} className="mt-8 space-y-6" action="send" method="POST">
+          <input type="hidden" name="remember" defaultValue="true" />
+          <div className="rounded-md shadow-sm -space-y-px">
+            <div className="flex mb-3">
+              <div className="w-1/2 mr-1">
+                <label htmlFor="firstname">
+                  Prénom
+            </label>
+                <input
+                  id="firstname"
+                  type="text"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="firstname"
+                />
+              </div>
+              <div class="w-1/2 ml-1">
+                <label htmlFor="lastname" >
+                  Nom
+            </label>
+                <input
+                  id="lastname"
+                  type="text"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="lastname"
+                />
+              </div>
+            </div>
+            <div>
+              <label htmlFor="phoneNumber" className="">
+                Numero de téléphone
+            </label>
+              <input
+                id="phoneNumber"
+                type="text"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="0610203040"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="email-address" className="">
+                Adresse Email
+              </label>
+              <input
+                id="email-address"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Email address"
+
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password" className="">
+                Mot de passe
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Password"
+
+              />
+            </div>
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-primary hover:bg-secondary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Créer
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/* eslint-disable */
+
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      primary: '#44a58a',
+      secondary: '#318D7D',
+      succes: '#0ab264',
+      info: '#0769ea',
+      warning: '#f4e404',
+      danger: '#e01d1d',
+      white: '#ffffff'
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
