7645aca45c9e5d608eb43c1c3bb9a0725a2d212b
JonathanGab <jogaborit@hotmail.com>
2021-06-04 12:13:19 +0200

    connect API with front-end

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
+import { ToastProvider } from 'react-toast-notifications';
+import './index.css';
 import Header from './components/Header';
 import Main from './components/Main';
 
 function App() {
   return (
-    <div className="App">
+    <ToastProvider>
       <Header />
       <Main />
-    </div>
+    </ToastProvider>
   );
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,24 +1,30 @@
 /* eslint-disable */
 import axios from 'axios';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useToasts } from 'react-toast-notifications';
 
-export default function SignUp() {
+export default function Login() {
   const { addToast } = useToasts();
+
   const apiBase = process.env.REACT_APP_API_BASE_URL;
   const { register, handleSubmit } = useForm();
-  const onSubmit = (form) => {
+  const onSubmit = ({ email, password }) => {
     axios
-      .post(`${apiBase}/users`, form)
-      .then((res) => addToast('La création de votre compte a été un succès !', { appearance: 'success', autoDismiss: true, autoDismissTimeout: 3000 }))
-      .catch((err) => addToast('Il y a eu une erreur lors de la création de votre compte.', { appearance: 'error', autoDismiss: true }))
-  };
+      .post(`${apiBase}/auth/login`, { email, password })
+      .then((res) => addToast('Connection réussie !', { appearance: 'success', autoDismiss: true, autoDismissTimeout: 3000 }))
+      .catch((err) => {
+        if (err.response && err.response.status === 401) {
+          addToast('Email ou mot de passe incorrect !', { appearance: 'error' });
+        } else window.console.error(err);
+      });
+  }
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-8 lg:px-8">
       <div className="max-w-md w-full">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Créer votre compte
+            Connecter vous
           </h2>
         </div>
         <form
@@ -29,44 +35,10 @@ export default function SignUp() {
         >
           <input type="hidden" name="remember" defaultValue="true" />
 
-          <div className="flex">
-            <div className="w-1/2 mr-1 mb-3">
-              <label htmlFor="firstname">Prénom</label>
-              <input
-                id="firstname"
-                type="text"
-                required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="firstname"
-                {...register('firstname')}
-              />
-            </div>
-            <div className="w-1/2 ml-1">
-              <label htmlFor="lastname">Nom</label>
-              <input
-                id="lastname"
-                type="text"
-                required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="lastname"
-                {...register('lastname')}
-              />
-            </div>
-          </div>
-          <div className="mb-3">
-            <label htmlFor="phoneNumber">Numero de téléphone</label>
-            <input
-              id="phoneNumber"
-              type="text"
-              className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-              placeholder="0610203040"
-              {...register('phone')}
-            />
-          </div>
           <div className="mb-3">
             <label htmlFor="email-address">Adresse Email</label>
             <input
-              id="email-address"
+
               name="email"
               type="email"
               autoComplete="email"
@@ -79,7 +51,7 @@ export default function SignUp() {
           <div className="mb-3">
             <label htmlFor="password">Mot de passe</label>
             <input
-              id="password"
+
               name="password"
               type="password"
               autoComplete="current-password"
@@ -89,17 +61,16 @@ export default function SignUp() {
               {...register('password')}
             />
           </div>
-
           <div>
             <button
               type="submit"
               className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-primary hover:bg-secondary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
-              Créer
+              Log
             </button>
           </div>
         </form>
-      </div>
-    </div>
+      </div >
+    </div >
   );
 }
